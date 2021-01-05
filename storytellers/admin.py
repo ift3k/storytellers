74600c87cc848c92811a1d9b30790cf5cd8f5362
tanj1m <tanjim1337@gmail.com>
2021-01-06 01:13:55 +0600

    Default model manager changed

--- storytellers/admin.py
+++ storytellers/admin.py
@@ -12,3 +12,5 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
+
+
--- storytellers/models.py
+++ storytellers/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 # Create your models here.
+
+##default manager 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset()\
+                                            .filter(status='published')
+
+##Post model
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -18,8 +26,12 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now = True)
     status = models.CharField(max_length = 10,choices = STATUS_CHOICES, default = 'draft')
 
+    objects = models.Manager()
+    published = PublishedManager()
+
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
